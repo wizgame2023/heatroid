9862a979a8495e6da8e036a32452cfcb7ad88938
Daiki Kamata <zeal23300159@fsgcl.jp>
2024-10-18 12:23:47 +0900

    Player移動とジャンプ判定

--- GameSources/Player.h
+++ GameSources/Player.h
@@ -9,7 +9,20 @@
 namespace basecross {
 
 	class Player : public GameObject {
+		//プレイヤーが使用するコントローラとキーボードの入力
+		Vec2 GetInputState() const;
+		// コントローラから方向ベクトルを得る
+		Vec3 GetMoveVector() const;
+		//プレイヤーの移動
+		void MovePlayer();
+		//文字列の表示
+		void DrawStrings();
+		//入力ハンドラー
+		InputHandler<Player> m_InputHandler;
+		//スピード
+		float m_Speed;
 
+		bool m_jamp;
 	public:
 		//構築と破棄
 
@@ -20,12 +33,12 @@ namespace basecross {
 		//初期化
 		virtual void OnCreate() override;
 		//更新
-		//virtual void OnUpdate() override;
+		virtual void OnUpdate() override;
 		//virtual void OnUpdate2() override;
-		//virtual void OnCollisionEnter(shared_ptr<GameObject>& Other) override;
+		virtual void OnCollisionEnter(shared_ptr<GameObject>& Other) override;
 		//virtual void OnCollisionExit(shared_ptr<GameObject>& Other) override;
 		////Aボタン
-		//void OnPushA();
+		void OnPushA();
 	};
 
 }
