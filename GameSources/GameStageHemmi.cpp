db8ef7e80a4f4634b9a0de1784ea0e56c61f4c1b
Daiki Kamata <zeal23300159@fsgcl.jp>
2024-10-18 11:39:07 +0900

    Stage追加

---
+++ GameSources/GameStageHemmi.cpp
@@ -0,0 +1,61 @@
+/*!
+@file GameStage.cpp
+@brief ゲームステージ実体
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+	//--------------------------------------------------------------------------------------
+	//	ゲームステージクラス実体
+	//--------------------------------------------------------------------------------------
+
+	//ビューとライトの作成
+	void GameStage::CreateViewLight() {
+		// カメラの設定
+		auto camera = ObjectFactory::Create<Camera>();
+		camera->SetEye(Vec3(0.0f, 2.0f, 15.0f));
+		camera->SetAt(Vec3(0.0f, 0.0f, 0.0f));
+
+		// ビューにカメラを設定
+		auto view = CreateView<SingleView>();
+		view->SetCamera(camera);
+
+		//マルチライトの作成
+		auto light = CreateLight<MultiLight>();
+		light->SetDefaultLighting(); //デフォルトのライティングを指定
+	}
+
+	void GameStage::CreateGameBox() {
+		//配列の初期化
+		vector< vector<Vec3> > vec = {
+			{
+				Vec3(30.0f, 1.0f, 3.0f),
+				Vec3(0.0f, 0.0f, 0.0f),
+				Vec3(0.0f, -4.0f, 0.0f)
+			},
+		};
+		//オブジェクトの作成
+		for (auto v : vec) {
+			AddGameObject<FixedBox>(v[0], v[1], v[2]);
+		}
+	}
+
+
+	void GameStage::OnCreate() {
+		try {
+			//ビューとライトの作成
+			CreateViewLight();
+
+			AddGameObject<Player>();
+			CreateGameBox();
+		}
+		catch (...) {
+			throw;
+		}
+	}
+
+}
+//end basecross
