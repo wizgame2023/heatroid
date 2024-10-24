14260cf349c3a86ee9f0c57340534121a3a57b29
Yuuya Tsuboi <zeal23300111@fsgcl.jp>
2024-10-24 11:35:39 +0900

    no message

--- GameSources/Player.h
+++ GameSources/Player.h
@@ -19,14 +19,21 @@ namespace basecross {
 		void ShowDebug();
 		//入力ハンドラー
 		InputHandler<Player> m_InputHandler;
-		//スピード
+		//スピード(最高速)
 		float m_speed;
+		//加速度
+		float m_accel;
+		//摩擦係数(地上)
+		float m_friction;
+		float m_frictionThreshold;
 		//ジャンプ高度
 		float m_jumpHeight;
 		//重力
 		float m_gravity;
 		//移動方向
 		Vec3 m_moveVel;
+		//CollisionExitの空中判定の閾値
+		int m_collideCount, m_collideCountInit;
 
 		enum Stats {
 			stand,		//地上
@@ -36,7 +43,7 @@ namespace basecross {
 
 		Stats m_stateType;
 
-		float _elapsed = App::GetApp()->GetElapsedTime();
+		float _delta = App::GetApp()->GetElapsedTime();
 
 	public:
 		//構築と破棄
@@ -49,11 +56,11 @@ namespace basecross {
 		virtual void OnCreate() override;
 		//更新
 		virtual void OnUpdate() override;
-		//virtual void OnUpdate2() override;
-		
+		virtual void OnUpdate2() override;
+
 		//何かに接触している判定(接地判定に活用？)
 		virtual void OnCollisionExcute(shared_ptr<GameObject>& Other) override;
-		
+
 		////Aボタン
 		void OnPushA();
 
