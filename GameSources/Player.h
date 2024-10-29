fc051667f8f56af051f8140e5b87cd7f41bf8fca
Yuuya Tsuboi <zeal23300111@fsgcl.jp>
2024-10-29 10:12:52 +0900

    TriggerCollクラスを全てCollision.h及びCollision.cppに移植

--- GameSources/Player.h
+++ GameSources/Player.h
@@ -10,50 +10,6 @@
 namespace basecross {
 
 //====================================================================
-// class TriggerColl
-// 押し合い処理を行わない球形コライダ
-//====================================================================
-
-	class TriggerColl : public Collision {
-	public:
-		TriggerColl(const shared_ptr<GameObject>& GameObjectPtr);
-		~TriggerColl();
-
-		float GetMadeDiameter() const;
-		void SetMadeDiameter(float f);
-		float GetMadeRadius() const;
-		void SetMadeRadius(float f);
-
-		void CollideTest(const shared_ptr<CollisionSphere>& DestColl);
-		CalcScaling GetCalcScaling() const;
-		void SetCalcScaling(CalcScaling s);
-		SPHERE GetSphere() const;
-		SPHERE GetBeforeSphere() const;
-		virtual bool SimpleCollisionCall(const shared_ptr<Collision>& Src) override;
-		virtual bool SimpleCollision(const shared_ptr<CollisionSphere>& DestColl) override;
-		virtual bool SimpleCollision(const shared_ptr<CollisionCapsule>& DestColl) override;
-		virtual bool SimpleCollision(const shared_ptr<CollisionObb>& DestColl) override;
-		virtual bool SimpleCollision(const shared_ptr<CollisionRect>& DestColl) override;
-		virtual void CollisionCall(const shared_ptr<Collision>& Src)override;
-		virtual void CollisionTest(const shared_ptr<TriggerColl>& DestColl)override;
-		virtual void CollisionTest(const shared_ptr<CollisionSphere>& DestColl)override;
-		virtual void CollisionTest(const shared_ptr<CollisionCapsule>& DestColl)override;
-		virtual void CollisionTest(const shared_ptr<CollisionObb>& DestColl)override;
-		virtual void CollisionTest(const shared_ptr<CollisionRect>& DestColl) override;
-		virtual bsm::Vec3 GetCenterPosition()const override;
-		virtual AABB GetEnclosingAABB()const override;
-		virtual AABB GetWrappedAABB()const override;
-
-		virtual void OnCreate() override;
-		virtual void OnUpdate() override;
-		virtual void OnDraw() override;
-
-	private:
-		struct Impl;
-		unique_ptr<Impl> pImpl;
-	};
-
-//====================================================================
 // class Player
 // プレイヤークラス
 //====================================================================
