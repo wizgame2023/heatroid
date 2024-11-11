81fa58c758d90d5816de04220f18b1898ea0ade4
Yuuya Tsuboi <zeal23300111@fsgcl.jp>
2024-11-12 08:30:00 +0900

    プレイヤーの操作処理を別関数に分けた 使ってなかったクラスの削除 HPゲージ（仮）

--- GameSources/Scene.cpp
+++ GameSources/Scene.cpp
@@ -32,7 +32,9 @@ namespace basecross{
 		app->RegisterResource(L"ENEMYARUKU", enemyAruki);
 
 		// 仮テクスチャ
+		app->RegisterTexture(L"White", texPath + L"White.png");
 		app->RegisterTexture(L"FIRE", texPath + L"fire_kari.png");
+		app->RegisterTexture(L"HEALTH", texPath + L"health.png");
 		app->RegisterTexture(L"CHARGE", texPath + L"charge.png");
 
 	}
@@ -47,7 +49,7 @@ namespace basecross{
 
 			//自分自身にイベントを送る
 			//これにより各ステージやオブジェクトがCreate時にシーンにアクセスできる
-			PostEvent(0.0f, GetThis<ObjectInterface>(), GetThis<Scene>(), L"ToGameStage");
+			PostEvent(0.0f, GetThis<ObjectInterface>(), GetThis<Scene>(), L"ToGameStageTsuboi");
 		}
 		catch (...) {
 			throw;
