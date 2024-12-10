f4131a9ebc03c1eb41ef41da928520e1e3993c32
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-12-10 09:56:36 +0900

    オーバーヒートの中の修正

--- GameSources/Enemyobj.cpp
+++ GameSources/Enemyobj.cpp
@@ -52,7 +52,7 @@ namespace basecross {
 		float elapsed = App::GetApp()->GetApp()->GetElapsedTime();
 		m_plungeTime -= elapsed;
 
-		if (!GetOverHeat()) {
+		if (!GetOverHeat() && m_stateType != wait) {
 			if (m_plungeTime >= 0.0f) {
 				EnemyAnime(L"spare");
 				SetState(m_attackState);
