30a9899a1b208885f6d9cd5d64a8837782a33d3f
Hemmi Akari <zeal23300133@fsgcl.jp>
2025-01-28 09:55:16 +0900

    レイキャスト制作途中

--- GameSources/Raycast.cpp
+++ GameSources/Raycast.cpp
@@ -7,5 +7,131 @@
 #include "Project.h"
 
 namespace basecross {
+	//--------------------------------------------------------------------------------------
+	//	class LineObject : public GameObject; //線を描画するオブジェクト
+	//--------------------------------------------------------------------------------------
+	LineObject::LineObject(const shared_ptr<Stage>& stage
+	):
+		GameObject(stage)
+	{}
+	LineObject::LineObject(const shared_ptr<Stage>& stage,
+		const shared_ptr<Player>& player,
+		const shared_ptr<Enemy>& enemy
+	) :
+		GameObject(stage),
+		m_player(player),
+		m_enemy(enemy),
+		m_direc(0.0f),
+		m_startColor(Col4(1.0f)),
+		m_endColor(Col4(1.0f))
+	{}
+	void LineObject::OnCreate() {
+		//線を構成する2点
+		m_vertices = {
+			{m_startPos, m_startColor},
+			{m_endPos, m_endColor}
+		};
+		//始点と終点をつなぐインデックス
+		m_indices = {
+			0,1
+		};
+
+		//描画
+		m_draw = AddComponent<PCStaticDraw>(); //位置と色のみ
+		m_draw->SetOriginalMeshUse(true); //自作したメッシュを使用
+		m_draw->CreateOriginalMesh(m_vertices, m_indices); //メッシュの作成
+		auto meshResoure = m_draw->GetMeshResource(); //メッシュリソースを取得し、プリミティブポロジー（頂点利用方法）を変更する
+		meshResoure->SetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_LINESTRIP); //ポリゴンではなく稜線を表示
+	}
+	void LineObject::OnUpdate() {
+		auto player = m_player.lock();
+		if (!player) return;
+		auto enemy = m_enemy.lock();
+		if (!enemy) return;
+
+		SetLinePosition(player->GetComponent<Transform>()->GetPosition(), enemy->GetWorldPos());
+		
+	}
+
+	//頂点の更新
+	void LineObject::VerticesUpdate() {
+		m_vertices = {
+			{m_startPos, m_startColor},
+			{m_endPos, m_endColor}
+		};
+
+		m_draw->UpdateVertices(m_vertices);
+	}
+
+	//頂点の設定
+	void LineObject::SetLinePosition(const Vec3& startPos, const Vec3& endPos) {
+		m_startPos = startPos;
+		m_endPos = endPos;
+		m_direc = endPos - startPos;
+		m_direc = m_direc.normalize();
+
+		VerticesUpdate();
+	}
+
+	//線の色の設定
+	void LineObject::SetLineColor(const Col4& startColor, const Col4& endColor) {
+		m_startColor = startColor;
+		m_endColor = endColor;
+
+		VerticesUpdate();
+	}
+
+	Vec3 LineObject::GetStartPos() {
+		return m_startPos;
+	}
+	Vec3 LineObject::GetEndPos() {
+		return m_endPos;
+	}
+	Vec3 LineObject::GetDirec() {
+		return m_direc;
+	}
+
+	RayMark::RayMark(const shared_ptr<Stage>& stage
+	):
+		GameObject(stage)
+	{}
+	void RayMark::OnCreate() {
+		auto draw = AddComponent<PCStaticDraw>();
+		draw->SetMeshResource(L"DEFAULT_SPHERE");
+		Col4 color(0.0f, 0.0f, 1.0f, 0.4f);
+		draw->SetDiffuse(color);
+		
+		SetAlphaActive(true);
+
+		m_trans = GetComponent<Transform>();
+		m_trans->SetScale(Vec3(1.0f));
+	}
+	void RayMark::OnUpdate() {
+		auto stage = GetStage();
+		Vec3 pos = Vec3(0.0f);
+		auto target = stage->GetSharedGameObject<Player>(L"Player");
+		auto line = stage->GetSharedGameObject<LineObject>(L"Line");
+
+		Vec3 rayStart = line->GetStartPos();
+		Vec3 rayEnd = line->GetEndPos();
+		Vec3 crossPos;
+
+		TRIANGLE triangle;
+		size_t triangleIndex;
+
+		auto draw = target->GetComponent<PNTStaticDraw>();
+		m_hitFlag = draw->HitTestStaticMeshSegmentTriangles(rayStart, rayEnd, crossPos, triangle, triangleIndex);
+
+		if (m_hitFlag) {
+			pos = crossPos;
+		}
+		m_trans->SetPosition(pos);
+	}
+	void RayMark::OnDraw() {
+		if (m_hitFlag) {
+			GameObject::OnDraw();
+		}
+	}
 }
+
 //end basecross
--- GameSources/Raycast.h
+++ GameSources/Raycast.h
@@ -1,10 +1,63 @@
-]/*!
+/*!
 @file Raycast.h
 @brief レイを飛ばす
 */
 
 #pragma once
 #include "stdafx.h"
+#include "Player.h"
+#include "Enemy.h"
 
 namespace basecross {
+	//--------------------------------------------------------------------------------------
+	//	class LineObject : public GameObject; //線を描画するオブジェクト
+	//--------------------------------------------------------------------------------------
+	class LineObject : public GameObject {
+	private:
+		Vec3 m_startPos;
+		Vec3 m_endPos;
+		Vec3 m_direc;
+
+		Col4 m_startColor;
+		Col4 m_endColor;
+
+		vector<VertexPositionColor> m_vertices;
+		vector<uint16_t> m_indices;
+		shared_ptr<PCStaticDraw> m_draw;
+
+		weak_ptr<Player> m_player;
+		weak_ptr<Enemy> m_enemy;
+
+	public:
+		LineObject(const shared_ptr<Stage>& stage);
+		LineObject(const shared_ptr<Stage>& stage, const shared_ptr<Player>& player,const shared_ptr<Enemy>& enemy);
+		virtual ~LineObject() {}
+		virtual void OnCreate() override;
+		virtual void OnUpdate() override;
+
+		void SetLinePosition(const Vec3& startPos, const Vec3& endPos);
+		void SetLineColor(const Col4& startColor, const Col4& endColor);
+		void VerticesUpdate();
+		Vec3 GetStartPos();
+		Vec3 GetEndPos();
+		Vec3 GetDirec();
+	};
+
+	//--------------------------------------------------------------------------------------
+	//	class RayMark : public GameObject; //オブジェクトに触れているかどうか
+	//--------------------------------------------------------------------------------------
+	class RayMark : public GameObject {
+	private : 
+		bool m_hitFlag;
+		shared_ptr<Transform> m_trans;
+	public:
+		RayMark(const shared_ptr<Stage>& stage);
+		virtual ~RayMark() {};
+
+		virtual void OnCreate() override;
+		virtual void OnUpdate() override;
+		virtual void OnDraw() override;
+	};
+
+
 }
